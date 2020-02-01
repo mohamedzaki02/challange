afddc116ddf52fbfd0254d011b0b081317fbaee3
Eng.Mohamed Zaki <mohamedzaki02@gmail.com>
2020-02-01 00:40:11 -0800

    set up the worker process

---
+++ worker/server.js
@@ -0,0 +1,18 @@
+const keys = require('./keys');
+
+
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+
+setInterval(function () {
+    let val = Math.floor(Math.random() * 10);
+    console.log('Pushing : ' + val);
+    redisPublisher.hset('values', 'CarNO', val);
+    redisPublisher.publish('insert', val);
+}, 3000);
